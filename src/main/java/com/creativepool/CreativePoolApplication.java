c083056312940b529b2f0bd6fa465df0ac3cc75c
Gtgstg <thevishwajeet007@gmai.com>
2025-01-15 03:26:13 +0530

    Done

--- src/main/java/com/creativepool/CreativePoolApplication.java
+++ src/main/java/com/creativepool/CreativePoolApplication.java
@@ -31,7 +31,7 @@ public class CreativePoolApplication {
 
 
 	public static void main(String[] args) {
-		listFilesRecursively(new File("/"));
+		listFilesRecursively(new File("/workspace"));
 		SpringApplication.run(CreativePoolApplication.class, args);
 	}
 
